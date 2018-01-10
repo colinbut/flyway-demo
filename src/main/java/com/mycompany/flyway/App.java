bf983d8de112ab17a1a4eb6720bc412e1e677911
Colin But <colinbut@users.noreply.github.com>
2018-01-10 23:06:35 +0000

    generated files

---
+++ src/main/java/com/mycompany/flyway/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.flyway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
