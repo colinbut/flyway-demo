a7a65594d2e899d5d36b143172fbcb9393b3626f
Colin But <colinbut@users.noreply.github.com>
2018-01-14 17:01:11 +0000

    migration script to add projects

---
+++ src/main/resources/db/migration/V5__Insert_Project_Data.sql
@@ -0,0 +1,5 @@
+insert into project (project_title, date_started, client_id) values ('project 1', DATE('2017-03-15'), 1);
+insert into project (project_title, date_started, client_id) values ('government project 1', DATE('2016-02-15'), 2);
+insert into project (project_title, date_started, date_ended, client_id) values ('financial project', DATE('2011-08-15'), DATE('2014-09-03'), 3);
+insert into project (project_title, date_started, client_id) values ('e-commerce project', DATE('2015-12-15'), 4);
+insert into project (project_title, date_started, client_id) values ('Project X', DATE('2017-06-15'), 5);
